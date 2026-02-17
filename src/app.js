59d213e2130aa5fb77f5fe376887e8b0f98bcc50
Sudheer kodavati <kodavatisudheer@gmail.com>
2026-02-17 20:38:18 +0530

    commiting query params

--- src/app.js
+++ src/app.js
@@ -1,10 +1,48 @@
 const express = require("express");
 
 const app = express();
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Sai Sudheer", lastName: "Kodavati" });
-});
+
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("1");
+      next();
+    },
+    (req, res, next) => {
+      console.log("2");
+      next();
+    },
+    (req, res, next) => {
+      console.log("3");
+      next();
+    },
+    (req, res, next) => {
+      console.log("4");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("5");
+    next();
+  },
+  (req, res, next) => {
+    console.log("6");
+    next();
+  },
+  (req, res, next) => {
+    console.log("7");
+    next();
+  },
+  (req, res, next) => {
+    console.log("8");
+    next();
+  },
+  (req, res, next) => {
+    console.log("9");
+    res.send("Route Handler");
+  },
+);
 
 app.listen(3000, () =>
   console.log("Server is successfully listening on port 3000...."),
